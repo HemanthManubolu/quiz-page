5963f5ac52db17a9568376bb03a1bd1ca456adf4
Hemanth Manubolu <160028440+HemanthManubolu@users.noreply.github.com>
2024-09-04 22:05:14 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,37 @@
+export const data = [
+    {
+        question:"Who is President of India?",
+        option1:"Narendra Modi ji",
+        option2:"Droupadi Murmu ji",
+        option3:"Rajanath Singh ji",
+        ans:2
+    },
+    {
+        question:"What is the capital of Japan?",
+        option1:"Mumbai",
+        option2:"Tokyo",
+        option3:"Singapore",
+        ans:2
+    },
+    {
+        question:"What is the fullform of UPSC",
+        option1:"United Public Secondary Comissioner",
+        option2:"United Police Comissioner",
+        option3:"United Public Service Commission",
+        ans:3
+    },
+    {
+        question:"What is the capital of Sikkim",
+        option1:"Gangtok",
+        option2:"Chennai",
+        option3:"Mumbai",
+        ans:1
+    },
+    {
+        question:"In which year India got independence?",
+        option1:"1947",
+        option2:"1948",
+        option3:"2024",
+        ans:1
+    },
+];
